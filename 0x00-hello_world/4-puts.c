f089b298027c1ed48abd83fb82264a33fe706ca9
Loay Ahmed <loayabelgendy@gmail.com>
2023-03-16 11:16:11 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main() {
+/**
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
+int main(void) {
 	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
 	return 0;
