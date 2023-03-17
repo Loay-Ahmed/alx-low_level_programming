bc8fd2745556f4f65b75055562fe04a318ed4ce7
Loay Ahmed <loayabelgendy@gmail.com>
2023-03-17 12:32:13 -0700

    1. The last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - entry point of program
+ *
+ *Return: return 0
+ */
+int main(void)
+{
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (int i = 0; i < 26; i++)
+		putchar(alpha[26]);
+
+	putchar('\n');
+}
