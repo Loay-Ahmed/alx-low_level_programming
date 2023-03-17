936d2da6c82c10658993713afdfd55c8cea9a33e
Loay Ahmed <loayabelgendy@gmail.com>
2023-03-17 13:01:42 -0700

    7. Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	int i = 26;
+	int i = 25;
 	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
 
-	for (i = 26; i >= 0; i--)
+	for (i = 25; i >= 0; i--)
 		putchar(alpha[i]);
 
 	putchar('\n');
