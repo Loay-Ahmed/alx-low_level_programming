b71c709e21a6ade15b18c7f44671e0634930901a
Loay Ahmed <loayabelgendy@gmail.com>
2023-03-17 12:58:25 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
