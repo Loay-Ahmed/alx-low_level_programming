7ad4de968fbe71fa9f5beee5a970ce54f02ec37e
Loay Ahmed <loayabelgendy@gmail.com>
2023-03-17 23:43:12 -0700

    1. The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int i, j, n;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			for (n = j + 1; n <= '9'; n++)
+			{
+	 			putchar(i);
+		 	 	putchar(j);
+				putchar(n);
+	 			if (i != '8')
+			 	{
+				putchar(',');
+				putchar(' ');
+			 	}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
