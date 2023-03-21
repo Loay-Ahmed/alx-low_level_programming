edd72cdf4694d90a6c0a15fbe104603b93fd471f
Loay Ahmed <loayabelgendy@gmail.com>
2023-03-21 11:12:16 -0700

    solving errors

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,13 +26,13 @@ void times_table(void)
 			}
 			else
 			{
-				_putchar(((i * j) / 10) + '0');
-				_putchar(((i * j) % 10) + '0');
 				if (j != 9)
 				{
 					_putchar(',');
 					_putchar(' ');
 				}
+				_putchar(((i * j) / 10) + '0');
+				_putchar(((i * j) % 10) + '0');
 			}
 		}
 		_putchar('\n');
