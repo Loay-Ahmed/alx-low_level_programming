b5c0a5af290b6a246c46c85d1b50c27c98f93664
Loay Ahmed <loayabelgendy@gmail.com>
2023-03-21 11:20:54 -0700

    solving errors

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,16 +9,16 @@
 void print_to_98(int n)
 {
 	int i;
-	if (n < 99)
+	if (n < 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 		{
 			printf("%d, ", n);
 		}
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i > 98; i--)
 		{
 			printf("%d, ", n);
 		}
