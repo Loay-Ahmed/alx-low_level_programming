ebbbdd4f0db5d4717a2a0ab789277b757aae66c4
Loay Ahmed <loayabelgendy@gmail.com>
2023-03-21 01:57:27 -0700

    task 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds to integers
+ *
+ * @i: first operand used in addition
+ * @j: second operan used in addition
+ *
+ * Return: returns the value of addition
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
