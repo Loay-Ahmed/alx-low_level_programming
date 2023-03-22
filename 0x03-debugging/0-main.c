ae7b59531a6dc59afbe9d8ba875d143a2af59ac7
Loay Ahmed <loayabelgendy@gmail.com>
2023-03-22 09:09:36 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - checks for correct output of postive_or_negative function
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+	i = 0;
+	postive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+# this directory is for debuging c code
