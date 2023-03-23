a22835e0648e7c9cec6d8c51a8ea91e39e1b0017
Loay Ahmed <loayabelgendy@gmail.com>
2023-03-23 07:29:57 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14, 10 times
+ *
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0, j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+			_putchar('0' + i);
+
+		_putchar('\n');
+	}
+}
