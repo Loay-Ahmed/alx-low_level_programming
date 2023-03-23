0123382832d9b9b2ec853398eab3852d3ba3f848
Loay Ahmed <loayabelgendy@gmail.com>
2023-03-23 07:14:28 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ *
+ * @a: first input
+ * @b: second input
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
