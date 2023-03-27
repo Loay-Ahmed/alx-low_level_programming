8aa755ea83e29eea0a3100c51d4e77b99c0bd62d
Loay Ahmed <loayabelgendy@gmail.com>
2023-03-27 12:03:33 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse to stdout
+ *
+ * @s: pointer to the string
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	s += _stlen(*s);
+	while ()
+	{
+		_putchar(*s);
+		s--;
+	}
+}
