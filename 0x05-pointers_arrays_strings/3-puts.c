cf5959530a37a957dba852711c639679360f3333
Loay Ahmed <loayabelgendy@gmail.com>
2023-03-27 11:33:00 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to the stdout
+ *
+ * @s: pointer to a string
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+		_putchar(*str);
+}
