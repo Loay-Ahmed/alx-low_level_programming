bfc92148ade47891031e5e43cc182537c51ce085
Loay Ahmed <loayabelgendy@gmail.com>
2023-03-27 21:32:26 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ *
+ * @str: pointer to a string
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int i, len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	for (i = (len - 1) / 2; i < len; i++)
+		_putchar(s[i]);
+
+	_putchar('\n');
+
+}
