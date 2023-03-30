ffdfac040453cb413851786d7f07fc132224d20f
Loay Ahmed <loayabelgendy@gmail.com>
2023-03-29 20:36:13 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcat - This function concatenate two strings
+ * dest, src in the dest
+ *
+ * @src: pointer to a string
+ * @dest: pointer to a string
+ * Return: the value othe value of  dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	*dest = *dest + *src;
+	return (*dest);
+}
