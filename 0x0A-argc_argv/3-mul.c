8faeea05718d83062da2714a6d4c7dc224551270
Loay Ahmed <loayabelgendy@gmail.com>
2023-04-10 00:14:16 -0700

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,34 @@
-#include "stdio.h"
 #include "main.h"
+#include "stdio.h"
+
+/**
+ * _atoi - converts the first portion of a string
+ * into integer
+ *
+ * @s: pointer to a string
+ * Return: 0 if there is no number,
+ * otherwise the number in the string
+ */
+int _atoi(char *s)
+{
+	int negative = 1, end = 0, num = 0;
+
+	while (*s)
+	{
+		if (*s == '-')
+			negative *= -1;
+		while (*s >= 48 && *s <= 57)
+		{
+			end = 1;
+			num = num * 10 + (*s - '0');
+			s++;
+		}
+		if (end == 1)
+			break;
+		s++;
+	}
+	return (num * negative);
+}
 
 /**
  * main - prints the product of two integers
@@ -10,11 +39,11 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc == 2)
+	if (argc == 3)
 	{
 		printf("%d\n", _atoi(argv[1]) * _atoi(argv[2]));
 		return (0);
 	}
-	printf("Error");
+	printf("Error\n");
 	return (1);
 }
