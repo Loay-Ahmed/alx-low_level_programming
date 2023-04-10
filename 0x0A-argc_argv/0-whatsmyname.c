d6273baeec25a8ed2187d2aac444e2207c345404
Loay Ahmed <loayabelgendy@gmail.com>
2023-04-09 22:17:37 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+ * main - Function that prints the name of the file
+ *
+ * @argc: number of arguments passed
+ * @argv: the values of the passed arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
