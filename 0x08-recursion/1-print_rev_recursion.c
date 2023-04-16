f47ce1af40cf952633e9d71241df2f2db027273c
Loay-Ahmed <loayabelgendy@gmail.com>
2023-04-15 23:40:27 -0700

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints the reverse of a string
+ *
+ * @s: pointer to the string
+ * Return: Nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_puts_recursion(++s);
+		_putchar(*s);
+	}
+}
