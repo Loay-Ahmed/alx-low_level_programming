a404567cb35f39e5f7b8911a9c79e5f0f6890d17
Loay-Ahmed <loayabelgendy@gmail.com>
2023-04-17 15:37:52 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - struct for dog
+ * @name: pointer to char
+ * @age: float
+ * @owner: pointer to char
+ *
+ * Description: struct for dog defining its name, age and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
