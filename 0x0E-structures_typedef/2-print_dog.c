8d0fe71ea55e52253b3b6e310039b18ed9fb8d46
Loay-Ahmed <loayabelgendy@gmail.com>
2023-04-17 16:04:38 -0700

    Task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints the name, age and owner
+ * of the struct dog
+ *
+ * @d: name of the struct
+ * Return: Nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name)
+			printf("Name: %s\n", d->name);
+		else
+			printf("(nil)\n");
+		if (d->age)
+			printf("Age: %f\n", d->age);
+		else
+			printf("(nil)\n");
+		if (d->owner)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("(nil)\n");
+	}
+}
