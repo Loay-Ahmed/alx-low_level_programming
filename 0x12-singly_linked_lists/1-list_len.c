a2837dd095ab415ff8a45e7c13aebecd89014381
Loay-Ahmed <loayabelgendy@gmail.com>
2023-04-28 12:56:15 -0700

    Task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * print_list - prints the values of the list
+ * list_len - prints the values of the list
  *
  * @h: pointer to the list
  * Return: number of nodes
  */
 
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t nodes = 0;
 
