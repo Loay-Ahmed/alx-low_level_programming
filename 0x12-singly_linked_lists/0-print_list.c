3bc4c3a43b13dc00e1d11eb6ae45fad3bc216f41
Loay-Ahmed <loayabelgendy@gmail.com>
2023-04-28 12:24:39 -0700

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - prints the values of the list
+ *
+ * @h: pointer to the list
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (h->str)
+		{
+			pritnf("[%d] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
