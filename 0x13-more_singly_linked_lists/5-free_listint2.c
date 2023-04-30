556295eeb5f4f783bc02bb0ec01c5c7ad2127725
Loay-Ahmed <loayabelgendy@gmail.com>
2023-04-30 13:02:12 -0700

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees the memory allocated for the list
+ *
+ * @head: the start of the list
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *current;
+
+	if (head)
+	{
+		current = *head;
+		while (current != NULL)
+		{
+			temp = current;
+			current = current->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
