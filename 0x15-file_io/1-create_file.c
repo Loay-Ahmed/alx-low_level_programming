434873b3c792b9241c7ca748104140cb02f82111
Loay-Ahmed <loayabelgendy@gmail.com>
2023-05-09 10:20:40 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,6 +21,8 @@ int create_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 
+	if (!text_content)
+		text_content = "";
 	for (letters = 0; text_content[letters] != '\0'; letters++)
 		;
 
